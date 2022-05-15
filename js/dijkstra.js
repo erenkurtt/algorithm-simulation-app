0596ed46bc5be39f6f5049b340248400291f041e
Beyza Koroglu <beyza.koroglu.97@gmail.com>
2022-05-15 22:06:21 +0300

    added dijkstra

--- js/dijkstra.js
+++ js/dijkstra.js
@@ -1,70 +1,236 @@
-
 nodesObjects = [];
+let nodesWeight = [];
 
 let dijkstra = {
-    'A': {'B': 2, 'C': 4},
-    'B': {'A': 2, 'C': 3, 'D': 8},
-    'C': {'A': 4, 'B': 3, 'E': 5, 'D': 2},
-    'D': {'B': 8, 'C': 2, 'E': 11, 'F':22},
-    'E': {'C': 5, 'D': 11, 'F': 1},
-    'F': {'D': 22, 'E': 1}
-}
+  A: { B: 2, C: 4 },
+  B: { C: 3, D: 8 },
+  C: { E: 5, D: 2 },
+  D: { E: 11, F: 22 },
+  E: { F: 1 },
+  F: {},
+};
 
 nodesCoordinates = [
-    ['A', 100, 250],
-    ['B', 300, 100],
-    ['C', 300, 400],
-    ['D', 500, 100],
-    ['E', 500, 400],
-    ['F', 700, 250],
+  ["A", 100, 250],
+  ["B", 300, 100],
+  ["C", 300, 400],
+  ["D", 500, 100],
+  ["E", 500, 400],
+  ["F", 700, 250],
+];
+
+let nodeWeight = [
+  ["A", 0],
+  ["B", -1],
+  ["C", -1],
+  ["D", -1],
+  ["E", -1],
+  ["F", -1],
 ];
 
 nodeLines = [];
 
 function startDijkstraGraph() {
+  canvasArea.stop();
 
-    canvasArea.stop();
+  canvasArea.start(updateCanvasDijkstraGraph);
 
-    canvasArea.start(updateCanvasDijkstraGraph);
-
-    for (let j = 0; j < nodesCoordinates.length; j++) {
-        let newObj = new componenttext(nodesCoordinates[j][0], nodesCoordinates[j][1], nodesCoordinates[j][2]);
-        nodesObjects.push(newObj);
+  for (let j = 0; j < nodesCoordinates.length; j++) {
+    if (
+      nodesCoordinates[j][0] === "A" ||
+      nodesCoordinates[j][0] === "C" ||
+      nodesCoordinates[j][0] === "E"
+    ) {
+      let newObj = new componenttext(
+        nodesCoordinates[j][0],
+        nodesCoordinates[j][1] - 15,
+        nodesCoordinates[j][2] + 20
+      );
+      nodesObjects.push(newObj);
+    } else {
+      let newObj = new componenttext(
+        nodesCoordinates[j][0],
+        nodesCoordinates[j][1],
+        nodesCoordinates[j][2]
+      );
+      nodesObjects.push(newObj);
     }
+  }
 
-    // for (let i = 0; j < kruskalArray.length; i++) {
-    //     // let obj = nodesCoordinates.filter(item => item[0])
-    // }
-    
-    let line = [];
+  for (let i = 0; i < nodeWeight.length; i++) {
+    if (
+      nodeWeight[i][0] === "A" ||
+      nodeWeight[i][0] === "C" ||
+      nodeWeight[i][0] === "E"
+    ) {
+      let weight = new componenttext(
+        nodeWeight[i][1] !== -1 ? nodeWeight[i][1] : "",
+        nodesCoordinates[i][1] - 35,
+        nodesCoordinates[i][2] + 35
+      );
+      nodesWeight.push(weight);
+    } else {
+      let weight = new componenttext(
+        nodeWeight[i][1] !== -1 ? nodeWeight[i][1] : "",
+        nodesCoordinates[i][1] + 15,
+        nodesCoordinates[i][2] - 20
+      );
+      nodesWeight.push(weight);
+    }
+  }
 
-    // kruskalArray.forEach(element => {
-    //     line = nodesCoordinates.filter(e => element[0] === e[0] || element[1] === e[0] )
-    //     edge = new componentLine( line[0][1], line[0][2], line[1][1], line[1][2] );
-    //     nodeLines.push(edge);
-    // })
-    
-    Object.keys(dijkstra).forEach( element => {
-        console.log(dijkstra[element])
-        Object.keys(dijkstra[element]).forEach ( e => {
-            console.log(e)
-            line = nodesCoordinates.filter(x => x[0] === e || x[0] === element)
-            edge = new componentLine( line[0][1], line[0][2], line[1][1], line[1][2], dijkstra[element][e] );
-            nodeLines.push(edge);
-        })
-    })
+  let line = [];
 
-    //deneme  =  new componenttext(  i , 10 * 5 * i, 350);
+  Object.keys(dijkstra).forEach((element) => {
+    Object.keys(dijkstra[element]).forEach((e) => {
+      line = nodesCoordinates.filter((x) => x[0] === e || x[0] === element);
+      edge = new componentLine(
+        line[0][1],
+        line[0][2],
+        line[1][1],
+        line[1][2],
+        dijkstra[element][e],
+        "black"
+      );
+      nodeLines.push(edge);
+    });
+  });
+
+  //deneme  =  new componenttext(  i , 10 * 5 * i, 350);
 }
+
+let keyCounter = -1;
+let valueCounter = 0;
+let keys = Object.keys(dijkstra);
+let lineCounter = 0;
 
 function updateCanvasDijkstraGraph() {
-
+  if (keyCounter < keys.length - 1 && keyCounter !== -1) {
     canvasArea.clear();
-    nodesObjects.forEach((element) => {
-        element.update();
-    })
-    nodeLines.forEach((element) => {
-        element.update();
-    })
-}
+    let currentPointWeight = nodeWeight.findIndex(
+      (e) => keys[keyCounter] === e[0]
+    );
 
+    valueKeys = Object.keys(dijkstra[keys[keyCounter]]);
+
+    console.log(valueKeys);
+    if (valueCounter < valueKeys.length) {
+    }
+
+    let nextPointWeight = nodeWeight.findIndex(
+      (x) => valueKeys[valueCounter] === x[0]
+    );
+    if (
+      nodeWeight[nextPointWeight][1] !== -1 &&
+      nodeWeight[nextPointWeight][1] !== 0
+    ) {
+      if (
+        nodeWeight[nextPointWeight][1] >
+        nodeWeight[currentPointWeight][1] +
+          dijkstra[keys[keyCounter]][valueKeys[valueCounter]]
+      ) {
+        nodeWeight[nextPointWeight][1] =
+          nodeWeight[currentPointWeight][1] +
+          dijkstra[keys[keyCounter]][valueKeys[valueCounter]];
+        nodeLines[lineCounter].color = "blue";
+      }
+    } else {
+      if (nodeWeight[nextPointWeight][1] === -1) {
+        nodeWeight[nextPointWeight][1] =
+          nodeWeight[currentPointWeight][1] +
+          dijkstra[keys[keyCounter]][valueKeys[valueCounter]];
+        nodeLines[lineCounter].color = "blue";
+      }
+    }
+
+    nodesWeight = [];
+
+    for (let i = 0; i < nodeWeight.length; i++) {
+      if (
+        nodeWeight[i][0] === "A" ||
+        nodeWeight[i][0] === "C" ||
+        nodeWeight[i][0] === "E"
+      ) {
+        let weight = new componenttext(
+          nodeWeight[i][1] !== -1 ? nodeWeight[i][1] : "",
+          nodesCoordinates[i][1] - 35,
+          nodesCoordinates[i][2] + 35
+        );
+        nodesWeight.push(weight);
+      } else {
+        let weight = new componenttext(
+          nodeWeight[i][1] !== -1 ? nodeWeight[i][1] : "",
+          nodesCoordinates[i][1] + 15,
+          nodesCoordinates[i][2] - 20
+        );
+        nodesWeight.push(weight);
+      }
+    }
+
+    nodesObjects.forEach((element) => {
+      element.update();
+    });
+
+    nodesWeight.forEach((element) => {
+      element.update("green");
+    });
+
+    nodeLines.forEach((element) => {
+      if (nodeLines[lineCounter].color === "black")
+        nodeLines[lineCounter].color = "red";
+      element.update();
+    });
+    // let temp = 0;
+    // for (let i = 0; i < nodeLines.length; i++) {
+    //   if (lineCounter === i) {
+    //       temp = i;
+    //     nodeLines[i].color = "blue";
+    //     nodeLines[i].update();
+    //   } else nodeLines[i].update();
+
+    //   nodeLines[temp].color = 'black';
+    // }
+
+    valueCounter++;
+    if (valueCounter === valueKeys.length) {
+      valueCounter = 0;
+      keyCounter++;
+    }
+    lineCounter++;
+  } else {
+    if (keyCounter === -1) {
+      canvasArea.clear();
+      nodesObjects.forEach((element) => {
+        element.update();
+      });
+
+      nodesWeight.forEach((element) => {
+        element.update("green");
+      });
+
+      nodeLines.forEach((element) => {
+        element.update();
+      });
+      keyCounter++;
+    } else {
+      //canvasArea.clear();
+      nodesObjects.forEach((element) => {
+        element.update();
+      });
+
+      nodesWeight.forEach((element) => {
+        element.update("green");
+      });
+
+      nodeLines.forEach((element) => {
+        if (element.color === "red") {
+          element.color = "white";
+          element.numberColor = "white";
+        }
+        element.update();
+        canvasArea.stop();
+      });
+    }
+  }
+  console.log(keyCounter);
+}
--- js/kruskal.js
+++ js/kruskal.js
@@ -1,125 +1,138 @@
-
 let nodesObjects = [];
 
 let kruskalArray = [
-    ['X', 'V', 1],
-    ['W', 'V', 1],
-    ['W', 'U', 1],
-    ['X', 'W', 2],
-    ['U', 'S', 2],
-    ['T', 'U', 3],
-    ['X', 'Y', 3],
-    ['U', 'V', 3],
-    ['S', 'T', 4],
-    ['Y', 'V', 4],
-    ['Y', 'T', 5],
-    ['Y', 'Z', 5],
-    ['Z', 'T', 10],
-  ];
+  ["X", "V", 1],
+  ["W", "V", 1],
+  ["W", "U", 1],
+  ["X", "W", 2],
+  ["U", "S", 2],
+  ["T", "U", 3],
+  ["X", "Y", 3],
+  ["U", "V", 3],
+  ["S", "T", 4],
+  ["Y", "V", 4],
+  ["Y", "T", 5],
+  ["Y", "Z", 5],
+  ["Z", "T", 10],
+];
 
 let nodesCoordinates = [
-    ['X', 100, 350],
-    ['V', 300, 250],
-    ['W', 300, 450],
-    ['U', 500, 450],
-    ['S', 700, 250],
-    ['T', 500, 150],
-    ['Y', 100, 150],
-    ['Z', 300, 50],
+  ["X", 100, 350],
+  ["V", 300, 250],
+  ["W", 300, 450],
+  ["U", 500, 450],
+  ["S", 700, 250],
+  ["T", 500, 150],
+  ["Y", 100, 150],
+  ["Z", 300, 50],
 ];
 
 let nodeLines = [];
 
 function startKruskalGraph() {
+  canvasArea.stop();
 
-    canvasArea.stop();
+  canvasArea.start(updateCanvasKruskalGraph);
 
-    canvasArea.start(updateCanvasKruskalGraph);
-
-    for (let j = 0; j < nodesCoordinates.length; j++) {
-        if(nodesCoordinates[j][0] === 'Y' || nodesCoordinates[j][0] === 'X' || nodesCoordinates[j][0] === 'W' || nodesCoordinates[j][0] === 'U'){
-            let newObj = new componenttext(nodesCoordinates[j][0], nodesCoordinates[j][1]-30, nodesCoordinates[j][2]+30);
-            nodesObjects.push(newObj);
-        }
-        else {
-            let newObj = new componenttext(nodesCoordinates[j][0], nodesCoordinates[j][1], nodesCoordinates[j][2]);
-            nodesObjects.push(newObj);
-        }
+  for (let j = 0; j < nodesCoordinates.length; j++) {
+    if (
+      nodesCoordinates[j][0] === "Y" ||
+      nodesCoordinates[j][0] === "X" ||
+      nodesCoordinates[j][0] === "W" ||
+      nodesCoordinates[j][0] === "U"
+    ) {
+      let newObj = new componenttext(
+        nodesCoordinates[j][0],
+        nodesCoordinates[j][1] - 30,
+        nodesCoordinates[j][2] + 30
+      );
+      nodesObjects.push(newObj);
+    } else {
+      let newObj = new componenttext(
+        nodesCoordinates[j][0],
+        nodesCoordinates[j][1],
+        nodesCoordinates[j][2]
+      );
+      nodesObjects.push(newObj);
     }
+  }
 
-    // for (let i = 0; j < kruskalArray.length; i++) {
-    //     // let obj = nodesCoordinates.filter(item => item[0])
-    // }
+  // for (let i = 0; j < kruskalArray.length; i++) {
+  //     // let obj = nodesCoordinates.filter(item => item[0])
+  // }
 
-    let line = [];
+  let line = [];
 
-    kruskalArray.forEach(element => {
-        line = nodesCoordinates.filter(e => element[0] === e[0] || element[1] === e[0] )
-        edge = new componentLine( line[0][1], line[0][2], line[1][1], line[1][2], element[2], 'black');
-        nodeLines.push(edge);
-    });
+  kruskalArray.forEach((element) => {
+    line = nodesCoordinates.filter(
+      (e) => element[0] === e[0] || element[1] === e[0]
+    );
+    edge = new componentLine(
+      line[0][1],
+      line[0][2],
+      line[1][1],
+      line[1][2],
+      element[2],
+      "black"
+    );
+    nodeLines.push(edge);
+  });
 
-    //deneme  =  new componenttext(  i , 10 * 5 * i, 350);
+  //deneme  =  new componenttext(  i , 10 * 5 * i, 350);
 }
 const usedNodes = [];
 const usedEdges = [];
 let i = 0;
-let colors = Array(kruskalArray.length).fill('black');
+let colors = Array(kruskalArray.length).fill("black");
 
 function updateCanvasKruskalGraph() {
+  canvasArea.clear();
+  nodesObjects.forEach((element) => {
+    element.update();
+  });
 
-    canvasArea.clear();
-    nodesObjects.forEach((element) => {
-        element.update();
-    });
+  if (i < kruskalArray.length) {
+    if (
+      usedNodes.includes(kruskalArray[i][0]) === false ||
+      usedNodes.includes(kruskalArray[i][1]) === false
+    ) {
+      // for(let k=0; k<kruskalArray.length; k++){
+      //     if(k==i)
+      //         nodeLines[k].update("blue");
+      //     else
+      //     nodeLines[k].update("black");
 
-    if (i < kruskalArray.length) {
-        
-        if(usedNodes.includes(kruskalArray[i][0]) === false || usedNodes.includes(kruskalArray[i][1]) === false){
-            for(let k=0; k<kruskalArray.length; k++){
-                if(k==i)
-                    nodeLines[k].update("blue");
-                else
-                nodeLines[k].update("black");
-                
-            }
+      // }
 
-            if(usedNodes.includes(kruskalArray[i][0]) === false){
-              usedNodes.push(kruskalArray[i][0]);
-              //colors[i] = 'blue';
-              nodeLines[i].color = 'blue';
-            }
-            if(usedNodes.includes(kruskalArray[i][1]) === false){
-              usedNodes.push(kruskalArray[i][1]);
-              //colors[i] = 'blue';
-              nodeLines[i].color = 'blue';
-            }
-        }
-        else {
-            //colors[i] = 'red';
-            nodeLines[i].color = 'red';
-        }
-
+      if (usedNodes.includes(kruskalArray[i][0]) === false) {
+        usedNodes.push(kruskalArray[i][0]);
+        //colors[i] = 'blue';
+        nodeLines[i].color = "blue";
+      }
+      if (usedNodes.includes(kruskalArray[i][1]) === false) {
+        usedNodes.push(kruskalArray[i][1]);
+        //colors[i] = 'blue';
+        nodeLines[i].color = "blue";
+      }
+    } else {
+      //colors[i] = 'red';
+      nodeLines[i].color = "red";
     }
+  }
+  nodeLines.forEach((element) => {
+    element.update();
+  });
+
+  if (i < kruskalArray.length) {
+    i++;
+  } else {
     nodeLines.forEach((element) => {
-        element.update();
+      if (element.color === "red") {
+        element.color = "white";
+        element.numberColor = "white";
+      }
+      element.update();
     });
-
-    if( i < kruskalArray.length){
-        i++;
-    }
-    else {
-        nodeLines.forEach((element) => {
-            if(element.color === 'red'){
-                element.color = 'white';
-                element.numberColor = 'white';
-            }
-            element.update();
-        });
-        canvasArea.stop();
-    }
-
+    canvasArea.stop();
+  }
 }
-
-
-
