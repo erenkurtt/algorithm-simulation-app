1643dbe57da7a6a3d82bef78d0bd299ef606070c
Beyza Koroglu <beyza.koroglu.97@gmail.com>
2022-06-06 00:44:49 +0300

    quickSort completed

--- js/quickSort.js
+++ js/quickSort.js
@@ -5,9 +5,9 @@ let equalArr = [];
 let rightArr = [];
 let quickCounter = 0;
 let red_num;
-let row = 1;
-let quickSortArr = [32, 15, 46, 2, 54, 6, 16, 98, 67, 103];
-let pivot_i = Math.floor(Math.random() * quickSortArr.length);
+let row = 0;
+let quickSortArr = [103, 32, 15, 98, 46, 2, 54, 6, 67];
+let pivot_i = 6; //Math.floor(Math.random() * quickSortArr.length);
 let pivot = quickSortArr[pivot_i];
 
 function startQuickSort() {
@@ -20,8 +20,8 @@ function startQuickSort() {
     quickCounter = 0;
     red_num;
     row = 1;
-    quickSortArr = [16, 15, 46, 2, 54, 6, 16, 98, 67, 103];
-    pivot_i = Math.floor(Math.random() * quickSortArr.length);
+    quickSortArr = [103, 32, 15, 98, 46, 2, 54, 6, 67];
+    pivot_i = 6; //Math.floor(Math.random() * quickSortArr.length);
     pivot = quickSortArr[pivot_i];
 
     canvasArea.stop();
@@ -29,17 +29,17 @@ function startQuickSort() {
     canvasArea.start(updateCanvasQuickSort);
 
     i = Math.floor((canvasArea.canvas.width/50 - quickSortArr.length)/2) ;
-    console.log(i);
     for (let j = 0; j < quickSortArr.length; j++) {
         let newObj = new componenttext(quickSortArr[j], 10 * 5 * i, 50);
-        //let newObj = new componenttext(quickSortArr[j], 10 * 5 * j, 50);
         quickArr.push(newObj);
-        console.log(i);
         i++;
     }
 
-    deneme  =  new componenttext(  i , 10 * 5 * i, 350);
+    line1  =  new componenttext(  "" , 150, 200);
+    line2  =  new componenttext(  "" , 300, 250);
+
 }
+
 
 function updateCanvasQuickSort() {
     quickArr.forEach(element => {
@@ -47,22 +47,20 @@ function updateCanvasQuickSort() {
     });
 
     //canvasArea.clear();
+    //if(quickCounter <= quickSortArr.length+2)
+        canvasArea.context.clearRect(0, 50*row, canvasArea.canvas.width, canvasArea.canvas.height)
 
-    canvasArea.context.clearRect(0, 50, canvasArea.canvas.width, canvasArea.canvas.height)
-
-    deneme.text = pivot;
-    deneme.x  =  10 * 5 * quickCounter;
-
-    deneme.update("blue");
+    if(quickCounter >= quickSortArr.length+2){
+        line1.text = "Now,  We  know  the  index  of  54  in  sorted  array."
+        line2.text = "Index  of  54 = 5"
+        line1.update("blue");
+        line2.update("blue");
+    }
 
     quickArr.forEach(element => {
         element.update("black");
     });
 
-    addRow(row);
-}
-
-function addRow (row) {
     let sortedArr = [];
 
     newArr=[
@@ -72,19 +70,36 @@ function addRow (row) {
         ...rightArr
     ];
 
-    console.log(newArr);
-
     i = canvasArea.canvas.width/100 - leftArr.length;
 
-    for (let j = 0; j < newArr.length; j++) {
-        let newObj = new componenttext(newArr[j], 10 * 5 * i, (row+1)*50);
-        sortedArr.push(newObj);
-        i++;
+    if(quickCounter < quickSortArr.length+2){
+        for (let j = 0; j < newArr.length; j++) {
+            let newObj = new componenttext(newArr[j], 10 * 5 * i, (row+1)*50);
+            sortedArr.push(newObj);
+            i++;
+        }
+    }
+    else{
+        for (let j = 0; j < newArr.length; j++) {
+            let x;
+            if(j<5)
+                x=i-1;
+            else{
+                if(j==5)
+                    x = i;
+                else
+                    x=i+1;
+            }
+
+            let newObj = new componenttext(newArr[j], 10 * 5 * x, (row+1)*50);
+            sortedArr.push(newObj);
+            i++;
+        } 
     }
 
     for(let i=0; i<sortedArr.length; i++){
 
-        if(i == red_num)
+        if(i == red_num && quickCounter<quickSortArr.length+2)
             sortedArr[i].update("red");
 
         else if(sortedArr[i].text==pivot)
@@ -108,21 +123,20 @@ function addRow (row) {
         equalArr.push(quickSortArr[quickCounter]);
         red_num = leftArr.length + equalArr.length;
     }
-    console.log(row);
 
-    if(quickCounter == quickSortArr.length){
-        if(row==1){
-            quickCounter=0;
-            leftArr = [];
-            rightArr = [];
-            equalArr = [];
-            row++;
-            console.log(row);
-            addRow(row);
-        }
-        else
-            canvasArea.stop();
+    if(quickCounter == quickSortArr.length+2){
+        canvasArea.stop();
+            // quickSortArr = [...leftArr];
+            // if(quickSortArr.length>1){
+
+            //     pivot_i = Math.floor(Math.random() * quickSortArr.length);
+            //     pivot = quickSortArr[pivot_i];
+            // }
+            // quickCounter=0;
+            // leftArr = [];
+            // rightArr = [];
+            // equalArr = [];
+            // row++;
     }
     quickCounter++;
-
 }
